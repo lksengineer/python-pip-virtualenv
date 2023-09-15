9e5b88f99b82d8cfd48e726d53eb2603e5739def
lksengineer <ksaavedrasu@gmail.com>
2023-09-14 19:16:28 -0500

    web-server

---
+++ web-server/main.py
@@ -0,0 +1,11 @@
+# Import store
+import store
+
+
+def run():
+    """Function Start"""
+    store.get_categories()
+
+
+if __name__ == '__main__':
+    run()
---
+++ web-server/store.py
@@ -0,0 +1,13 @@
+# Import requests
+import requests
+
+
+def get_categories():
+    """Function Get Categories"""
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categories = r.json()
+    for category in categories:
+        print(category['name'])
