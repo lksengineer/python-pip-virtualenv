10b10cc82e2b88969588b14c9111cdfed0324f54
lksengineer <ksaavedrasu@gmail.com>
2023-09-14 20:16:42 -0500

    modified: add FastAPI Web Frameword and Uvicorn Server

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,28 @@
 # Import store
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+# Comando para ejecutar uvicorn
+# uvicorn main:app --reload
+@app.get('/')
+def get_list():
+    return [1, 2, 3]
+
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list2():
+    return """
+        <h1>Hola soy una pagina</h1>
+        <p>soy un parrafo</p>
+    """
+
+
+@app.get('/contacts')
+def get_list3():
+    return {"Name": "LKS Code"}
 
 
 def run():
